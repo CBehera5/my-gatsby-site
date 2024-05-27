bf72d2d5b7af57162ffab115f099515746d379db
CBehera5 <chinmaybehera08@gmail.com>
2024-05-27 10:42:28 +0530

    WIP

---
+++ gatsby-config.mjs
@@ -0,0 +1,25 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const config = {
+  siteMetadata: {
+    title: `New Relic Assignment`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-mdx",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+  ],
+};
+
+export default config;
